6f7a9ad77a27e42bfffa0221cac7d895455c6b57
Andrew Goetz <erax0r@gmail.com>
2023-03-11 22:29:18 -0800

    implement history

--- home/views.py
+++ home/views.py
@@ -13,37 +13,26 @@ import datetime
 TEMPLATE_DIRS = ('home/templates',)
 
 def index(request):
-    # create a datetime object for the current timestamp
     timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     
     if request.method == 'POST':
         load_dotenv() # Load all the ENV variables into your os enviroment.
         user_input = request.POST.get('user_input', '')
-        hidden_input = request.POST.get('my_hidden_input', '')
-        if user_input.strip():
-            msgs = []
-            if hidden_input.strip():
-                # Convert the hidden_input string to a Python object
-                history = json.loads(hidden_input)                
-                # Append the history to the msgs list
-                # is there a better way to do this, its slow and inefficient, does not scale
-                for msg in history:
-                    msgs.append(msg)
-            # Append the user input to the msgs list
-            # msgs.append({"role": "user", "content": user_input,"timestamp":timestamp})
-            msgs.append({"role": "user", "content": user_input})
+        hidden_input = request.POST.get('conversation', '')
+        if user_input.strip():            
+            if hidden_input:
+                conversation = json.loads(hidden_input)                
+            else:
+                conversation = []
+            conversation.append({"role": "user", "content": user_input})
             openai.api_key = os.getenv("OPENAI_API_KEY")
-            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=msgs,user="test123")
-            # response = openai.ChatCompletion.create(model="davinci-similarity", messages=msgs)
+            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=conversation,user="test123")
             timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             reply = response["choices"][0]["message"]["content"].lstrip()
-            # Append the AI response to the msgs list
-            # msgs.append({"timestamp":timestamp,"role": "assistant", "content": reply})
-            msgs.append({"role": "assistant", "content": reply})
-            # Convert the msgs list to a JSON string
-            msgs_json = json.dumps(msgs)
-            logRequest(request,msgs) #log the conversation
-            return render(request, "index.html", {"data": reply, "data2": msgs_json})
+            conversation.append({"role": "assistant", "content": reply})
+            logRequest(request,conversation) #log the conversation
+            msgsjson = json.dumps(conversation)            
+            return render(request, "index.html", {"data": reply, "data2": conversation, "data3": msgsjson})
     return render(request, "index.html", {"data": ""})
 
 def logRequest(request,msgs):
